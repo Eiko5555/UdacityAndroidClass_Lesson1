694b58424dbdc99841ed5f119eaca40e80e70388
econMinnie <etcontest7@gmail.com>
2016-11-03 17:34:03 -0700

    for Udacity android
    Lesson 1 - Single Screen App
    Lesson 2-

---
+++ app/src/main/java/com/example/eiko/cookies/SingleScreenApp.java
@@ -0,0 +1,15 @@
+package com.example.eiko.cookies;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by eiko on 11/3/2016.
+ */
+public class SingleScreenApp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_screen_app);
+    }
+}
